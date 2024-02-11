5077334016e34fd2d804f782e88b8e16b1989809
George Voronin <bordernormal@gmail.com>
2024-02-12 03:09:23 +0700

    Push gradle

--- app/src/main/java/hexlet/code/app/controller/WelcomeController.java
+++ app/src/main/java/hexlet/code/app/controller/WelcomeController.java
@@ -1,2 +1,17 @@
-package hexlet.code.app.controller;public class WelcomeController {
+package hexlet.code.app.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/welcome")
+public class WelcomeController {
+    @GetMapping("")
+    @ResponseStatus(HttpStatus.OK)
+    public String welcome() {
+        return "Welcome to Spring";
+    }
 }
