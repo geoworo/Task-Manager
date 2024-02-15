a20d10afe0099c284fd425e84afb0511f10d992c
George Voronin <bordernormal@gmail.com>
2024-02-16 04:17:49 +0700

    Implenent authentication

--- app/src/main/java/hexlet/code/controller/UserController.java
+++ app/src/main/java/hexlet/code/controller/UserController.java
@@ -4,9 +4,11 @@ import hexlet.code.dto.UserCreateDTO;
 import hexlet.code.dto.UserUpdateDTO;
 import hexlet.code.service.UserService;
 import hexlet.code.dto.UserDTO;
+import hexlet.code.util.UserUtils;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,7 +18,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.PathVariable;
-
 import java.util.List;
 
 @RestController
@@ -24,6 +25,9 @@ import java.util.List;
 public class UserController {
     @Autowired
     UserService us;
+
+    @Autowired
+    UserUtils uu;
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
@@ -46,12 +50,22 @@ public class UserController {
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public UserDTO update(@Valid @RequestBody UserUpdateDTO data, @PathVariable Long id) {
-        return us.update(data, id);
+        var currentUser = uu.getCurrentUser();
+        if (currentUser.getId().equals(id)) {
+            return us.update(data, id);
+        } else {
+            throw new AccessDeniedException("Access denied");
+        }
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
-        us.delete(id);
+        var currentUser = uu.getCurrentUser();
+        if (currentUser.getId().equals(id)) {
+            us.delete(id);
+        } else {
+            throw new AccessDeniedException("Access denied");
+        }
     }
 }
--- app/src/main/java/hexlet/code/service/UserService.java
+++ app/src/main/java/hexlet/code/service/UserService.java
@@ -3,6 +3,7 @@ package hexlet.code.service;
 import hexlet.code.dto.UserCreateDTO;
 import hexlet.code.dto.UserDTO;
 import hexlet.code.dto.UserUpdateDTO;
+import hexlet.code.util.UserUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
